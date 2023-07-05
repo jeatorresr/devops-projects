50796fae818cd23a23fdf978bd3a5d0fc48d0100
jeatorresr <jandreatorresro@outlook.com>
2023-07-05 16:15:26 -0500

    :speech_balloon: Exercise 2 - bash scripting

--- Shell-scripting/bash_ex2.sh
+++ Shell-scripting/bash_ex2.sh
@@ -1,9 +1,8 @@
 #! /bin/bash
-
-#Create a bash script that would display the calendar of given year.
-#The script would prompt the user to enter a year; then, it would display the corresponding year’s calendar.
+# Exercise 2
+# Create a bash script that would display the calendar of given year.
+# The script would prompt the user to enter a year; then, it would display the corresponding year’s calendar.
 
 echo "Calendar"
-
 read -p "Please, type the year: " YEAR
 cal $YEAR
