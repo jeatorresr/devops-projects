69a786f18a7fc2ffcd2201acb98525f38f585e5b
jeatorresr <jandreatorresro@outlook.com>
2023-07-05 16:15:48 -0500

    :speech_balloon: Exercise 3 - bash scripting

--- Shell-scripting/bash_ex3.sh
+++ Shell-scripting/bash_ex3.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
-
-#Create a bash script that would display a file content in upper case letters.
-#The file path must be passed to the script as an argument.
+# Exercise 3
+# Create a bash script that would display a file content in upper case letters.
+# The file path must be passed to the script as an argument.
 
 echo "Displaying Content of $1 in upper case."
 cat $1 | tr [:lower:] [:upper:]
