5bc9fa5048469898b44765ff77d5fee6df569c08
jeatorresr <jandreatorresro@outlook.com>
2023-07-05 12:43:04 -0500

    :speech_balloon: Exercise 1 - bash scripting

---
+++ Shell-scripting/bash_ex1.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+# Create a bash script named cores.sh that prints out the number of CPU cores you have in your system.
+
+echo "CPU cores"
+
+# command to know how many cpu cores I have on my Mac.
+sysctl -n hw.ncpu
